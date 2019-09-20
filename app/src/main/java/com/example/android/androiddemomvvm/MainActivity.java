5a8d5bba518fcfe789da561a2e67a70c26645d49
pedrohjc <pedrohj.carvalho@gmail.com>
2019-09-20 11:11:39 -0400

    Finished

--- app/src/main/java/com/example/android/androiddemomvvm/MainActivity.java
+++ app/src/main/java/com/example/android/androiddemomvvm/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.android.androiddemomvvm;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.androiddemomvvm.Interface.LoginResultCallBack;
+import com.example.android.androiddemomvvm.ViewModel.LoginViewModel;
+import com.example.android.androiddemomvvm.ViewModel.LoginViewModelFactory;
+import com.example.android.androiddemomvvm.databinding.ActivityMainBinding;
+
+import es.dmoral.toasty.Toasty;
+
+public class MainActivity extends AppCompatActivity implements LoginResultCallBack {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setViewModel(ViewModelProviders.of(this,new LoginViewModelFactory(this))
+                .get(LoginViewModel.class));
+    }
+
+    @Override
+    public void onSuccess(String message) {
+        Toasty.success(this, "Login Success", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    public void onError(String message) {
+        Toasty.error(this, "Login Error", Toast.LENGTH_SHORT)
+                .show();
     }
 }
